c5bcb19f664b7e7a1f8daba50663cb8ce8971a06
ashutoshkumar2021 <80633589+ashutoshkumar2021@users.noreply.github.com>
2024-10-13 11:58:39 +0530

    .

---
+++ client/build/precache-manifest.5faae990b5d5f95d3e2c7f3db746cb64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030feee67eab30d7945730b157ea2798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c4f222cf756ef4d431c",
+    "url": "/static/css/main.838fa79b.chunk.css"
+  },
+  {
+    "revision": "bfac1060db75104a6e9c",
+    "url": "/static/js/2.c584c563.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c584c563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4f222cf756ef4d431c",
+    "url": "/static/js/main.36dee8d2.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
